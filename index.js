b9206493dd3dfe5bc6b4409e4cab6fecbbe42791
Sofía <168145697+sofiadfc@users.noreply.github.com>
2024-09-25 21:31:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("buttonText").addEventListener("click", function(){
+    const text = document.getElementById("inputText").value;
+    localStorage.setItem("data", text);
+})
